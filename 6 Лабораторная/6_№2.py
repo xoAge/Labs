742506cf80a1d32b0515ed1687351b3038235213
xoAge <124346068+xoAge@users.noreply.github.com>
2023-05-20 00:51:43 +0400

    Update 6_№2.py

--- 6 Лабораторная/6_№2.py
+++ 6 Лабораторная/6_№2.py
@@ -2,30 +2,35 @@
 #минимум одно ограничение на характеристики объектов и целевую функцию для оптимизации решения.
 
 #У каждого зала есть номер, рейтинг, стоимость, экскурсия проходит только по четным залам.
-#Сформировать вариант прохода экскурсии по 2 залам с минимальной стоимостью, соотвественно с минимальным рейтингом.
-K = int(input("Введите количество залов, от 3: "))
-while K < 3:
-    K = int(input("Введите количество залов, от 3: "))
+#Сформировать вариант прохода экскурсии по 2 залам с минимальной стоимостью, соответственно с наименьшим рейтингом.
+
+import random
+from random import randint
+
+K = int(input("Введите количество залов, от 3 до 50: "))
+while K < 3 or K > 50:
+    K = int(input("Введите количество залов, от 3 до 50: "))
 ekskursia = []
-for i in range(1, K + 1):
-    nomer = int(input("\nВведите номер зала, экскурсии только по четным залам: "))
-    while nomer % 2 != 0 or nomer == 0:
-        nomer = int(input("Введите четный номер зала: "))
-    reiting = int(input("Введите рейтинг зала: "))
-    cost = int(input("Введите стоимость посещения этого зала, посещение не может стоить больше 200: "))
-    while cost > 200:
-        cost = int(input("Посещение не может стоить больше 200: "))
-    ekskursia.append((nomer, reiting, cost))
-max_reiting = 1000
+min_reiting = min_cost = 10000
 zal_1 = zal_2 = 0
-max_cost = 1000
+nomer = []
+while len(nomer) < K:
+    nome = randint(1, 101)
+    if nome not in nomer and nome % 2 == 0:
+        nomer.append(nome)
+        print("\nНомер зала: |",nome,"|")
+        reiting = random.randint(1, 10)
+        print("Рейтинг зала: ", reiting)
+        cost = random.randint(100, 200)
+        print("Стоимость зала: ", cost)
+        ekskursia.append((nome, reiting, cost))
 for i in range(K):
     for j in range(K):
         if i != j:
             cost_sum = ekskursia[i][2] + ekskursia[j][2]
             reiting_sum = ekskursia[i][1] + ekskursia[j][1]
-            if reiting_sum < max_reiting and cost_sum < max_cost:
-                max_cost = cost_sum
+            if reiting_sum < min_reiting and cost_sum < min_cost:
+                min_cost = cost_sum
                 max_reiting = reiting_sum
                 zal_1 = ekskursia[i][0]
                 zal_2 = ekskursia[j][0]
