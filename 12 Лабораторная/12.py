29dc1802690f0ffafd9906de3a96a51d736907cd
xoAge <124346068+xoAge@users.noreply.github.com>
2023-12-16 23:11:36 +0400

    Update 12.py

--- 12 Лабораторная/12.py
+++ 12 Лабораторная/12.py
@@ -6,18 +6,20 @@ from decimal import Decimal, getcontext
 def count_summ(matrix, t):
     t = 1 / (10 ** t)
     result_matrix = matrix.copy()
-    i, fact_n, summa, result_num = 1, 1, 1, 0
+    n, fact_n, summa, result_num = 1, 1, 1, 0
     while abs(summa) > t:           # Цикл для вычисления суммы ряда
-        fact_n *= i
+        fact_n *= n
         result_matrix = np.dot(np.dot(result_matrix, matrix), matrix)
         matrix_det = Decimal(np.linalg.det(result_matrix))
         summa = (matrix_det*Decimal(fact_n)) / Decimal(fact_n)
-        result_num += -abs(summa) if i == 1 else summa * (-1) ** i
-        i += 1
-    print(f"Количество итераций: {i - 1}")
+        result_num += -abs(summa) if n == 1 else summa * (-1) ** n
+        n += 1
+    print(f"Количество итераций: {n - 1}")
     return result_num
 try:        # Ввод точности t
-    t = abs(int(input("Введите точность, число t (количество знаков после запятой): ")))
+    t = int(input("Введите точность, число t (количество знаков после запятой): "))
+    while t > 100 or t < 1:  # Проверка допустимости введенной точности
+        t = int(input("Введите число t, большее или равное 1 и меньшее 100:\n"))
     rank = np.random.randint(2, 10) # Генерация случайной матрицы
     matrix = np.random.uniform(-1,1, size = (rank, rank))
     getcontext().prec = t + 100
@@ -25,7 +27,8 @@ try:        # Ввод точности t
     Rang = np.linalg.matrix_rank(matrix)  # Вычисление ранга матрицы x.
     print(f"Сгенерированная матрица:\n {matrix} \nРанг матрицы: {Rang}")
     summa_tot = count_summ(matrix, t)
+    rounded_summa = round(summa_tot, t)
     print(f'Заданная точность: {t}')
-    print(f"Сумма ряда: {summa_tot}")
+    print(f"Сумма ряда: {rounded_summa}")
 except ValueError:
     print("\nВведенный символ не является числом. Перезапустите программу и введите число.")
