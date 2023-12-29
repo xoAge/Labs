85f31a1cf6b3fcc4600a69550f295126fd339096
xoAge <124346068+xoAge@users.noreply.github.com>
2023-12-29 18:57:31 +0400

    Update 12.py

--- 12 Лабораторная/12.py
+++ 12 Лабораторная/12.py
@@ -2,33 +2,28 @@
 #n - номер слагаемого. Сумма считается вычисленной, если точность вычислений будет не меньше t знаков после запятой.
 #У алгоритма д.б. линейная сложность. Знак первого слагаемого  -.
 import numpy as np
-from decimal import Decimal, getcontext
-def count_summ(matrix, t):
-    t = 1 / (10 ** t)
-    result_matrix = matrix.copy()
-    n, fact_n, summa, result_sum = 1, 1, 1, 0
-    while abs(summa) > t:           # Цикл для вычисления суммы ряда
-        fact_n *= n
-        result_matrix = np.dot(np.dot(result_matrix, matrix), matrix)
-        matrix_det = Decimal(np.linalg.det(result_matrix))
-        summa = (matrix_det*Decimal(fact_n)) / Decimal(fact_n)
-        result_sum += -abs(summa) if n == 1 else summa * (-1) ** n
+def result_sum(det_x, t):
+    n,fact,current_sum = 1,1,0
+    chislitel = det_x * (fact*n)
+    while True:
+        fact = fact*n # Вычисляем факториал в числителе
+        summa = chislitel / fact # Вычисляем очередное слагаемое
+        current_sum += -abs(summa) if n == 1 else summa * (-1) ** n
         n += 1
-    print(f"Количество итераций: {n - 1}")
-    return result_sum
-try:        # Ввод точности t
-    t = int(input("Введите точность, число t (количество знаков после запятой): "))
-    while t > 100 or t < 1:  # Проверка допустимости введенной точности
-        t = int(input("Введите число t, большее или равное 1 и меньшее 100:\n"))
-    rank = np.random.randint(2, 10) # Генерация случайной матрицы
-    matrix = np.random.uniform(-1,1, size = (rank, rank))
-    getcontext().prec = t + 100
+        if summa < t:  # Проверяем точность
+            break
+    return current_sum
+try:
+    t = int(input("Введите желаемую точность вычислений (не равную нулю): "))
+    while t > 50 or t < 1:
+        t = int(input("Введите число t, большее или равное 1 и меньшее 50:\n"))
+    k = np.random.randint(2, 9)
     np.set_printoptions(linewidth=200)
-    Rang = np.linalg.matrix_rank(matrix)  # Вычисление ранга матрицы x.
-    print(f"Сгенерированная матрица:\n {matrix} \nРанг матрицы: {Rang}")
-    summa_tot = count_summ(matrix, t)
-    rounded_summa = round(summa_tot, t)
-    print(f'Заданная точность: {t}')
-    print(f"Сумма ряда: {rounded_summa}")
+    random_matrix = np.random.uniform(-1, 1, (k, k))
+    print("Сгенерирована матрица:\n", random_matrix)
+    det_x = np.linalg.det(random_matrix)
+    result = result_sum(det_x, t)
+    precision_format = "{:." + str(int(t)) + "f}"
+    print(f"Сумма знакопеременного ряда: {precision_format.format(result)}")
 except ValueError:
     print("\nВведенный символ не является числом. Перезапустите программу и введите число.")
